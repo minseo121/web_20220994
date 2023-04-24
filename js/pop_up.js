5219edb8248c34af95b8fd9027c461e38586eba3
민서 <kittse1217@gmail.com>
2023-04-24 16:34:29 +0000

    왜 안되냐잇

--- js/pop_up.js
+++ js/pop_up.js
@@ -1,24 +1,24 @@
-function pop_up(){
-	window.open("../pop_up/pop_up.html","팝업테스트","width=400,height=300,top=10,left=10");
-}
 function show_clock(){
-	let currentDate = new Date(); // 날짜 객체 생성
-    let divClock = document.getElementById('divClock');
-	let msg="현재 시간 : ";
-	if(currentDate.getHours()>12){
+	let currentDate = new Date(); //날짜 객체 생성
+	let divClock = document.getElementById('divClock');
+	let msg = "현재 시간: ";
+	if(currentDate.getHours()>12){ //12시보다 크면 오후
 		msg += "오후";
 		msg += currentDate.getHours()-12+"시";
-	}
-	else{
+	}else{
 		msg += "오전";
 		msg += currentDate.getHours()+"시";
 	}
+	
 	msg += currentDate.getMinutes()+"분";
 	msg += currentDate.getSeconds()+"초";
 	divClock.innerText = msg;
 	
-	if(currentDate.getMinutes()>58){
+	if(currentDate.getMinutes()>58){ //정각 1분 전 빨강색 출력
 		divClock.style.color="red";
 	}
-	setTimeout(show_clock, 1000);
+	setTimeout(show_clock, 1000); //1초마다 갱신
 }
+function pop_up(){
+	window.open("../pop_up/pop_up.html", "팝업 테스트", "width=400, height=300 top=10, left = 10");
+} //윈도우.오픈() 함수는 창을 열거나 닫는(.close)전용 함수이다.
--- js/search.js
+++ js/search.js
@@ -3,6 +3,14 @@ document.getElementById("search_btn").addEventListener('click', search_message);
 function search_message(){
    alert("검색을 수행합니다!"); 
 	let search_str = document.querySelector("#search_txt"); // 변수에 저장
-   	document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
-   	console.log(search_str.value); // 콘솔에 출력
+	if(search_str.value.length){
+		alert("검색어가 비었습니다. 입력해주세요.")
+	}
+	else{
+		alert("검색을 수행합니다!");
+		let text = document.getElementById("search_message").innerHTML=search_str.value;
+		document.querySelector("#form_main").submit();
+		}	
+	//document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
+   //console.log(search_str.value); // 콘솔에 출력
 }
