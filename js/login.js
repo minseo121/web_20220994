715537ef4751c07c8ca7ba11b6dfa85870f61538
민서 <kittse1217@gmail.com>
2023-05-17 02:15:55 +0000

    dddd

---
+++ js/login.js
@@ -0,0 +1,118 @@
+function login(){
+    let form = document.querySelector("#form_main");
+    let id = document.querySelector("#floatingInput");
+    let password = document.querySelector("#floatingPassword");
+	let check = document.querySelector("#idSaveCheck");
+    
+    
+    form.action = "../index_login.html";
+    form.method = "get"
+    
+	if(check.checked == true) { // 아이디 체크 o
+            alert("쿠키를 저장합니다.");
+            setCookie("id", id.value, 1); // 1일 저장
+            alert("쿠키 값 :" + id.value);
+        } 
+    else { // 아이디 체크 x
+            setCookie("id", id.value, 0); //날짜를 0 - 쿠키 삭제
+    }
+
+	
+    if(id.value.length === 0 || password.value.length === 0){
+        alert("아이디와 비밀번호를 모두 입력해주세요.");
+    }else{
+		session_set(); // 세션 생성
+        form.submit();
+		//return true;
+    }
+}
+function get_id(){
+    var getParameters = function(paramName){ // 변수 = 함수(이름)
+    var returnValue; // 리턴값을 위한 변수 선언
+    var url = location.href; // 현재 접속 중인 주소 정보 저장
+    var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&'); // ?기준 slice 한 후 split 으로 나눔
+        for(var i = 0; i < parameters.length; i++) { 
+          var varName = parameters[i].split('=')[0];
+            
+            if (varName.toUpperCase() == paramName.toUpperCase()) {
+                returnValue = parameters[i].split('=')[1];
+                return decodeURIComponent(returnValue);
+            // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
+          }
+       } // 2중 for문 끝
+}; // 함수 끝
+alert(getParameters('id') + '님 반갑습니다!'); // 메시지 창 출력
+}
+
+function logout(){
+	session_del(); // 세션 삭제
+	location.href="index.html";
+}
+
+function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString();        
+    }
+
+function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+            var cookie_array = cookie.split("; ");
+            for ( var index in cookie_array) {
+                var cookie_name = cookie_array[index].split("=");
+                
+                if (cookie_name[0] == "id") {
+                    return cookie_name[1];
+                }
+            }
+        }
+        return ;
+}
+function session_set() { //세션 저장
+    let id = document.querySelector("#floatingInput");
+    if (sessionStorage) {
+        sessionStorage.setItem("Session_Storage_test", id.value);
+
+    } else {
+        alert("로컬 스토리지 지원 x");
+    }
+}
+
+function session_get() { //세션 읽기
+    if (sessionStorage) {
+       return sessionStorage.getItem("Session_Storage_test");
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+function session_check() { //세션 검사
+    if (sessionStorage.getItem("Session_Storage_test")) {
+        alert("이미 로그인 되었습니다.");
+        location.href='index_login.html'; // 로그인된 페이지로 이동
+    }
+}
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+   let id = document.querySelector("#floatingInput");
+   let check = document.querySelector("#idSaveCheck");
+   let get_id = getCookie("id");
+    
+   if(get_id) { 
+      id.value = get_id; 
+      check.checked = true; 
+   }
+   session_check();   //세션 유무 검사
+}
+function session_del() {//세션 삭제
+    // Check if the sessionStorage object exists
+    if (sessionStorage) {
+        // Retrieve data
+        sessionStorage.removeItem("Session_Storage_test");
+        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+
