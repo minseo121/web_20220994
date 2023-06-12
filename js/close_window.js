0a9dbe15682833a3f77699153dbb793a56dcaf59
민서 <kittse1217@gmail.com>
2023-06-12 14:20:35 +0000

    일단 7주차 응용문제까지는 완료!

--- js/close_window.js
+++ js/close_window.js
@@ -1,16 +1,17 @@
-var close_time;//시간 정보
-var close_time2=10;
+var close_time;
+var close_time2 = 10;
 
-clearTimeout(close_time); //재호출 정지
+clearTimeout(close_time);
 close_time=setTimeout("close_window()", 10000);
 show_time();
 
 function show_time(){
-	let divClock = document.getElementById('Time');
-	divClock.innerText=close_time2;
+	let divClock = document.getElementById("Time");
+	divClock.innerText="남은 시간은" + close_time2+ "초입니다.";
 	close_time2--;
 	setTimeout(show_time, 1000);
 }
+
 function close_window(){
 	window.close();
 }
--- js/search.js
+++ js/search.js
@@ -1,8 +1,33 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
+var search_array=[];
+
 function search_message(){
-	alert("검색을 수행합니다!");
 	let search_str = document.querySelector("#search_txt"); // 변수에 저장
-	document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
-    console.log(search_str.value); // 콘솔에 출력
+	if(search_str.value.length===0){
+		alert("검색어가 비었습니다. 입력해주세요.");
+	}
+	else{
+		if(no_str(search_str.value)){ // 검색어가 적절한지 확인합니다.
+			alert("검색을 수행합니다.");
+			search_array.push(search_str.value);
+			if (search_array.length > 10) {
+                search_array.shift(); // 배열의 맨 앞의 값을 삭제합니다.
+            }
+			let text=document.getElementById("search_message").innerHTML = search_array.toString(); // 태그에 값 추가
+			document.querySelector("#form_main").submit();
+		} else {
+			alert(search_str.value + "는 검색어로 적절하지 않습니다.");
+		}
+	}
+}
+
+function no_str(search_str){
+	const banned_words = ['욕설1', '욕설2', '욕설3']; // 필터링할 비속어를 배열에 저장합니다.
+	for(let i=0; i<banned_words.length; i++){
+		if(search_str.includes(banned_words[i])){
+			return false;
+		}
+	}
+	return true;
 }
