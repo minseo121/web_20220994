895d91ec61cb89873f4f0e26b58d91c7de362977
민서 <kittse1217@gmail.com>
2023-06-13 13:20:09 +0000

    지도중

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
-        center: new kakao.maps.LatLng(37.380493180533705, 126.92774330111054 ), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(위도, 경도), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
@@ -16,4 +16,3 @@ map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성
 var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
